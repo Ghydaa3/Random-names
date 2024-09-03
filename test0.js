3ec0275f04478e77a4f6ec8ef446c9fa01dd8da5
Ghydaa3 <ghydaa.abuzainh3@gmail.com>
2024-09-04 00:29:17 +0300

    select random name

--- test0.js
+++ test0.js
@@ -1,40 +1,71 @@
 let names = [
-    "Alice", "Bob", "Charlie", "Diana",
-    "Fiona", "George", "Hannah", "Isaac", "Jasmine","Maya","Louis",
-    "Kevin", "Laura", "Mason", "Nina", "Oscar",
-    "Rachel", "Sam", "Tina","Selena", "Seth","Frank","Mason"
-]
+  "Alice",
+  "Bob",
+  "Charlie",
+  "Diana",
+  "Fiona",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "Maya",
+  "Louis",
+  "Kevin",
+  "Laura",
+  "Mason",
+  "Nina",
+  "Oscar",
+  "Rachel",
+  "Sam",
+  "Tina",
+  "Selena",
+  "Seth",
+  "Frank",
+  "Mason",
+];
 
 let alphabet = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-    "U", "V", "W", "X", "Y", "Z"
-]
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
+let randomindex = Math.floor(Math.random() * alphabet.length);
+let selectname = alphabet[randomindex];
 
-
-let randomindex=Math.floor(Math.random()*alphabet.length)
-let selectname=alphabet[randomindex]
-
-console.log(selectname)
+console.log(selectname);
 
 function fname() {
-    for (let i = 0; i < names.length; i++) {
-       
-        if(names[i][0]==selectname){
-            console.log('welcome ' + names[i])}
-
-            else {
-                console.log('sorry baby')
-            }
-
-        
-    } 
-
-    
-
-   
-    
+  for (let i = 0; i < names.length; i++) {
+    if (names[i][0] == selectname) {
+      console.log("welcome " + names[i]);
+    } else {
+      console.log("Sorry but this alphabet not found");
+    }
+  }
 }
 
-fname(selectname)
+fname(selectname);
